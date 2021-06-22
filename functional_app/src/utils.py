729ec6a6b11d34d0c3988b37b490300462e934ec
Chris de la Cruz <77891283+cdlc01@users.noreply.github.com>
2021-06-22 14:06:38 -0400

    Update utils.py
    cleaned up code

--- functional_app/src/utils.py
+++ functional_app/src/utils.py
@@ -4,18 +4,10 @@ import pickle
 def extract_feature_values(data):
     """ Given a params dict, return the values for feeding into a model"""
     
-    # Replace these features with the features for your model. They need to 
-    # correspond with the `name` attributes of the <input> tags
-    
     with open('src/feature_names.pkl', 'rb') as f:
         expected_features = pickle.load(f)
     
     EXPECTED_FEATURES = expected_features
-
-    # This assumes all inputs will be numeric. If you have categorical features
-    # that the user enters as a string, you'll want to rewrite this as a for
-    
-    # loop that treats different features differently
     
     values = []
     for feature in EXPECTED_FEATURES:
