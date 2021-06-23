7e6cf52a78be122494192eed68e89c01957272ee
Chris de la Cruz <77891283+cdlc01@users.noreply.github.com>
2021-06-23 14:21:40 -0400

    Update utils.py
    exp #20

--- Hate_Speech_Predictor_Application/src/utils.py
+++ Hate_Speech_Predictor_Application/src/utils.py
@@ -4,18 +4,20 @@ import pickle
 def extract_feature_values(data):
     """ Given a params dict, return the values for feeding into a model"""
     
-    with open('src/feature_names.pkl', 'rb') as f:
-        expected_features = pickle.load(f)
+    #with open('src/feature_names.pkl', 'rb') as f:
+        #expected_features = pickle.load(f)
     
-    EXPECTED_FEATURES = expected_features
+    #EXPECTED_FEATURES = expected_features
     
-    values = []
-    for feature in EXPECTED_FEATURES:
-        if feature in data:
-            values.append(1)
-        else:
-            values.append(0)
+    #values = []
+    #for feature in EXPECTED_FEATURES:
+        #if feature in data:
+            #values.append(1)
+        #else:
+            #values.append(0)
             
-    new_values = [values]
+    #new_values = [values]
     
-    return pd.DataFrame(new_values, columns=EXPECTED_FEATURES)
+    #return pd.DataFrame(new_values, columns=EXPECTED_FEATURES)
+    
+    return [[data]]
